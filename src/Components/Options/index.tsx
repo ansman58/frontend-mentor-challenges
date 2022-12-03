9e8159a06dd4b9f9768c3d52cd64334a7f2ab482
JAVA <Anslem Nnakwe>
2022-12-03 17:37:28 +0100

    design player options screen

---
+++ src/Components/Options/index.tsx
@@ -0,0 +1,45 @@
+import Lizard from "../../assets/icon-lizard.svg";
+import style from "./Options.module.scss";
+import clsx from "clsx";
+
+interface IOptions {
+  className?: string;
+  icon: any;
+  isScissors?: boolean;
+  isPaper?: boolean;
+  isRock?: boolean;
+  isSpock?: boolean;
+  isLizard?: boolean;
+}
+
+const Options: React.FC<IOptions> = ({
+  className,
+  icon,
+  isScissors,
+  isPaper,
+  isRock,
+  isSpock,
+  isLizard,
+}) => {
+  return (
+    <div className={style.wrapper}>
+      <div
+        className={clsx(
+          style.container,
+          {
+            [style.rock]: isRock,
+            [style.paper]: isPaper,
+            [style.scissors]: isScissors,
+            [style.spock]: isSpock,
+            [style.lizard]: isLizard,
+          },
+          className
+        )}
+      >
+        <img src={icon} alt="" className={clsx(style.img)} />
+      </div>
+    </div>
+  );
+};
+
+export default Options;
--- src/Components/Ready/index.tsx
+++ src/Components/Ready/index.tsx
@@ -1,12 +1,51 @@
-// import { ReactComponent as PentagonBg } from '../../assets/bg-pentagon.svg'
+import PentagonBg from "../../assets/bg-pentagon.svg";
 import style from "./Ready.module.scss";
-import { ReactComponent as Scissors } from "../../assets/icon-scissors.svg";
+import Scissors from "../../assets/icon-scissors.svg";
+import Lizard from "../../assets/icon-lizard.svg";
+import Spock from "../../assets/icon-spock.svg";
+import Paper from "../../assets/icon-paper.svg";
+import Rock from "../../assets/icon-rock.svg";
+import Options from "../Options";
+import clsx from "clsx";
 
 const Ready = () => {
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
-        <Scissors />
+        <div className={style.content}>
+          <div className={style["img-container"]}>
+            <img
+              src={PentagonBg}
+              alt="background-image"
+              className={style["bg-image"]}
+            />
+          </div>
+          <Options
+            icon={Scissors}
+            className={clsx(style.scissors, style.option)}
+            isScissors
+          />
+          <Options
+            icon={Paper}
+            className={clsx(style.paper, style.option)}
+            isPaper
+          />
+          <Options
+            icon={Rock}
+            className={clsx(style.rock, style.option)}
+            isRock
+          />
+          <Options
+            icon={Lizard}
+            className={clsx(style.lizard, style.option)}
+            isLizard
+          />
+          <Options
+            icon={Spock}
+            className={clsx(style.spock, style.option)}
+            isSpock
+          />
+        </div>
       </div>
     </div>
   );
